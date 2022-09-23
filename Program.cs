3b766470e199901c8de7ed6e9a636256c0cb4fba
zacharykollar <77861842+zacharykollar@users.noreply.github.com>
2022-09-23 17:51:01 -0400

    Removed legacy console code from program.cs

--- Program.cs
+++ Program.cs
@@ -9,34 +9,6 @@ namespace ClassManagerApp
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form());
-            Course course = new Course("Math");
-            Controller.AddCourse(course);
-            course.AddTask(new Task("first"));
-            course.AddTask(new Task("second"));
-            course.AddTask(new Task("third"));
-            Controller.ShowCourseTasks();
-            while (true)
-            {
-                string ex = Console.ReadLine();
-                switch (ex)
-                {
-                    case "ShowCourseTasks":
-                        Controller.ShowCourseTasks();
-                        break;
-                    case "AddCourse":
-                        Controller.AddCourse();
-                        break;
-                    case "AddTask":
-                        Controller.AddTask();
-                        break;
-                    case "Exit":
-                        Environment.Exit(0);
-                        break;
-                    default:
-                        Console.WriteLine("Invalid Command");
-                        break;
-                }
-            }
         }
     }
 }
