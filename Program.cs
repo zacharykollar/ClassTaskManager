0808a9c03f07f0d96f70ef72aa64fbdf8f5a7749
zacharykollar <77861842+zacharykollar@users.noreply.github.com>
2022-09-23 14:12:24 -0400

    Modified program to be main-style and exist within ClassManagerApp namespace

--- Program.cs
+++ Program.cs
@@ -1,31 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-using ClassManagerApp;
-
-Course course = new Course("Math");
-Controller.AddCourse(course);
-course.AddTask(new ClassManagerApp.Task("first"));
-course.AddTask(new ClassManagerApp.Task("second"));
-course.AddTask(new ClassManagerApp.Task("third"));
-Controller.ShowCourseTasks();
-while (true)
+﻿namespace ClassManagerApp
 {
-    string ex = Console.ReadLine();
-    switch (ex)
+    internal class Program
     {
-        case "ShowCourseTasks":
+        private static void Main(string[] args)
+        {
+            Course course = new Course("Math");
+            Controller.AddCourse(course);
+            course.AddTask(new Task("first"));
+            course.AddTask(new Task("second"));
+            course.AddTask(new Task("third"));
             Controller.ShowCourseTasks();
-            break;
-        case "AddCourse":
-            Controller.AddCourse();
-            break;
-        case "AddTask":
-            Controller.AddTask();
-            break;
-        case "Exit":
-            Environment.Exit(0);
-            break;
-        default:
-            Console.WriteLine("Invalid Command");
-            break;
+            while (true)
+            {
+                string ex = Console.ReadLine();
+                switch (ex)
+                {
+                    case "ShowCourseTasks":
+                        Controller.ShowCourseTasks();
+                        break;
+                    case "AddCourse":
+                        Controller.AddCourse();
+                        break;
+                    case "AddTask":
+                        Controller.AddTask();
+                        break;
+                    case "Exit":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Command");
+                        break;
+                }
+            }
+        }
     }
 }
