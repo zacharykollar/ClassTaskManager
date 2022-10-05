737c7a28e7e2a42a45c0246797f46aaa9cb28ebf
zacharykollar <77861842+zacharykollar@users.noreply.github.com>
2022-10-04 20:28:16 -0400

    Added: CourseControl usercontrol

---
+++ CourseControl.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace ClassManagerApp
+{
+    partial class CourseControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelClassName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelClassName
+            // 
+            this.labelClassName.AutoSize = true;
+            this.labelClassName.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelClassName.Location = new System.Drawing.Point(0, 0);
+            this.labelClassName.Name = "labelClassName";
+            this.labelClassName.Size = new System.Drawing.Size(182, 32);
+            this.labelClassName.TabIndex = 0;
+            this.labelClassName.Text = "labelClassName";
+            // 
+            // ClassControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.labelClassName);
+            this.Name = "ClassControl";
+            this.Size = new System.Drawing.Size(275, 286);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label labelClassName;
+    }
+}
---
+++ CourseControl.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassManagerApp
+{
+    public partial class CourseControl : UserControl
+    {
+        private Course course;
+        public CourseControl(Course c)
+        {
+            Course = c;
+            InitializeComponent();
+        }
+
+        public Course Course { get => course; set => course = value; }
+    }
+}
