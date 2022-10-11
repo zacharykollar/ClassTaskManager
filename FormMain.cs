d2d1d2acaec08f988e59b584ba326c240f3c3c41
zacharykollar <77861842+zacharykollar@users.noreply.github.com>
2022-10-11 14:09:21 -0400

    Clean up for FormMain

--- FormMain.cs
+++ FormMain.cs
@@ -1,14 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace ClassManagerApp
+﻿namespace ClassManagerApp
 {
     public partial class FormMain : Form
     {
@@ -38,7 +28,7 @@ namespace ClassManagerApp
             tabControlCourses.TabPages.Clear();
             for (int i = 0; i < controller.Courses.Count; i++)
             {
-                TabPage tb = new TabPage(controller.Courses[i].Name)
+                TabPage tb = new (controller.Courses[i].Name)
                 {
                     AutoScroll = true
                 };
